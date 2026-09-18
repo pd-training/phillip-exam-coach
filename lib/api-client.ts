a9f2be54674fd07923518d9a914660647d828980
training <jjolyn.t@gmail.com>
2026-09-18 14:58:13 +0800

    Create api-client.ts

---
+++ lib/api-client.ts
@@ -0,0 +1,18 @@
+export async function apiFetch(
+  url: string,
+  options: RequestInit = {}
+): Promise<any> {
+  const response = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    ...options,
+  });
+
+  if (!response.ok) {
+    throw new Error(`API error: ${response.status}`);
+  }
+
+  return response.json();
+}
