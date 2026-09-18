885c1c7d2253574cffa655c9d06534e6e7a50dac
training <jjolyn.t@gmail.com>
2026-09-18 14:56:19 +0800

    Update and rename tsconfig.json to next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "avatars.githubusercontent.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
