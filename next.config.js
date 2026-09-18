52091f8136a1020ee57458ab348f961e597befdf
training <jjolyn.t@gmail.com>
2026-09-18 10:26:15 +0800

    Configure remote image patterns in next.config.js
    Add remote image patterns for GitHub and Google avatars

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
