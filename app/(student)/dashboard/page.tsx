5e3d5ad945ca296997a6b2f0a977d9f4836e113c
training <jjolyn.t@gmail.com>
2026-09-18 14:28:23 +0800

    Update page.tsx

--- app/(student)/dashboard/page.tsx
+++ app/(student)/dashboard/page.tsx
@@ -3,7 +3,6 @@
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
-import Link from "next/link";
 
 interface Paper {
   id: string;
